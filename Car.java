4806c3096a84ffed0150c211f35225e0486ddf34
mameevdi <mameevs@gmail.com>
2023-01-10 00:39:35 +0300

    Задача 2

--- Car.java
+++ Car.java
@@ -1,2 +1,23 @@
+//Задание 2. Напишите программу, которая будет выводить в консоль информацию о каждом автомобиле и все характеристики.
 public class Car {
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int productionYear;
+    String productionCountry;
+
+    Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
+
+    @Override
+    public String toString() {
+        return brand + " " + model + " " + productionYear + " года выпуска, сборка в " + productionCountry + ", " + color + " цвет кузова, объем двигателя — " + engineVolume + " л.";
+    }
 }
